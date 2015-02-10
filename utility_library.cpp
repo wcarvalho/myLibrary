59bbc2eb436ba0545819294d8aec4aaa164e801f
Wilka Carvalho <wilka.carvalho@stonybrook.edu>
2015-02-10 13:27:22 -0500

    update

--- utility_library.cpp
+++ utility_library.cpp
@@ -5,15 +5,21 @@ using namespace std;
 namespace wul
 {
 	std::string appendStrings(std::string string1, std::string string2, std::string string3, std::string string4, std::string string5)
-	{	
+	{
 		std::stringstream ss; ss.str("");
 		ss << string1 << string2 << string3 << string4 << string5;
 		return ss.str();
 	}
+
 	std::string stringDouble(std::string s, double d)
 	{
 		stringstream ss; ss.str("");
 		ss << s << d;
 		return ss.str();
+	}
+
+	void vectorminmax(std::vector<double> const& x, double &min, double &max){
+		min = *min_element(x.begin(), x.end());
+		max = *max_element(x.begin(), x.end());
 	}
 }
--- utility_library.h
+++ utility_library.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>      // std::stringstream
+#include <vector>
 
 namespace wul
 {
@@ -10,5 +11,7 @@ namespace wul
 	std::string appendStrings(std::string string1="", std::string string2="", std::string string3="", std::string string4="", std::string string5="");
 	// append double to string
 	std::string stringDouble(std::string s, double d);
+	void vectorminmax(std::vector<double> const& x, double &min, double &max);
 }
+
 #endif
